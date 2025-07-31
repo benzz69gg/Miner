d2930e35cfdc4859328bbc43ba20fa825c5e3db3
benzz69gg <sbenz7009@gmail.com>
2025-08-01 04:42:02 +0700

    sclip

---
+++ minerkid.sh
@@ -0,0 +1,15 @@
+yes | pkg update && pkg upgrade -y
+yes | pkg install libjansson build-essential clang binutils git -y
+
+cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
+git clone https://github.com/Darktron/ccminer.git
+cd ccminer
+chmod +x build.sh configure.sh autogen.sh start.sh
+
+CXX=clang++ CC=clang ./build.sh
+
+rm -rf config.json
+
+wget https://github.com/benzz69gg/Miner/blob/main/config.json
+
+~/ccminer/start.sh
